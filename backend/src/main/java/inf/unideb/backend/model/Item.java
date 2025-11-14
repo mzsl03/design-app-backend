9504dbe61716bf36b8474eb9013d7f68a1584cc3
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-14 09:14:14 +0100

    feat: model for design item added created

---
+++ backend/src/main/java/inf/unideb/backend/model/Item.java
@@ -0,0 +1,24 @@
+package inf.unideb.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "ITEMS")
+public class Item {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String title;
+    private String imageUrl;
+    @Column(length = 2000)
+    private String description;
+    private String tags;
+}
