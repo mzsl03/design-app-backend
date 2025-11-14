a67d4148fab9019572369cbc5af86d5d9de219ab
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-14 10:38:40 +0100

    feat: add http request handler controllers for board entity

---
+++ backend/src/main/java/inf/unideb/backend/controller/BoardController.java
@@ -0,0 +1,28 @@
+package inf.unideb.backend.controller;
+
+import inf.unideb.backend.model.Board;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BoardController {
+
+    @GetMapping("/api/boards")
+    List<Board> getAll();
+
+    @GetMapping("/api/boards/{id}")
+    Board getOne(@PathVariable UUID id);
+
+    @PostMapping("/api/boards")
+    Board create(@RequestBody Board board);
+
+    @PutMapping("/api/boards/{id}")
+    Board update(@PathVariable UUID id, @RequestBody Board board);
+
+    @DeleteMapping("/api/boards/{id}")
+    void delete(@PathVariable UUID id);
+
+    @PostMapping("/api/boards/{boardId}/addItem/{itemId}")
+    Board addItem(@PathVariable UUID boardId, @PathVariable UUID itemId);
+}
---
+++ backend/src/main/java/inf/unideb/backend/controller/BoardControllerImpl.java
@@ -0,0 +1,61 @@
+package inf.unideb.backend.controller;
+
+import inf.unideb.backend.model.Board;
+import inf.unideb.backend.model.Item;
+import inf.unideb.backend.repository.BoardRepository;
+import inf.unideb.backend.repository.ItemRepository;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+public class BoardControllerImpl implements BoardController {
+
+    private final BoardRepository boardRepository;
+    private final ItemRepository itemRepository;
+
+    public BoardControllerImpl(BoardRepository boardRepository, ItemRepository itemRepository) {
+        this.boardRepository = boardRepository;
+        this.itemRepository = itemRepository;
+    }
+
+    @Override
+    public List<Board> getAll() {
+        return boardRepository.findAll();
+    }
+
+    @Override
+    public Board getOne(UUID id) {
+        return boardRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public Board create(Board board) {
+        return boardRepository.save(board);
+    }
+
+    @Override
+    public Board update(UUID id, Board board) {
+        Board existing = boardRepository.findById(id).orElseThrow();
+
+        existing.setName(board.getName());
+        existing.setUser(board.getUser());
+
+        return boardRepository.save(existing);
+    }
+
+    @Override
+    public void delete(UUID id) {
+        boardRepository.deleteById(id);
+    }
+
+    @Override
+    public Board addItem(UUID boardId, UUID itemId) {
+        Board board = boardRepository.findById(boardId).orElseThrow();
+        Item item = itemRepository.findById(itemId).orElseThrow();
+
+        board.getItems().add(item);
+        return boardRepository.save(board);
+    }
+}
