f825621d5c4eb411fcf911adc80f72e47fdef409
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-14 10:22:10 +0100

    feat: add user model entity

---
+++ backend/src/main/java/inf/unideb/backend/model/User.java
@@ -0,0 +1,21 @@
+package inf.unideb.backend.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity(name = "USERS")
+public class User {
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+    private String username;
+    private String email;
+}
