3ada879c892ee4dd1dc2875d5aba0bec34aa90e9
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-27 11:36:41 +0100

    feat: add method findByUsername to accommodate spring security

--- backend/src/main/java/inf/unideb/backend/repository/UserRepository.java
+++ backend/src/main/java/inf/unideb/backend/repository/UserRepository.java
@@ -4,6 +4,8 @@ import inf.unideb.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByUsername(String username);
 }
