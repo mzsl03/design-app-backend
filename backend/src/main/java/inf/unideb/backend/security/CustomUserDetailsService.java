1bd6ec19e951267a2ba1687382281b413998bf23
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-27 11:38:20 +0100

    feat: add security impl to project

---
+++ backend/src/main/java/inf/unideb/backend/security/CustomUserDetailsService.java
@@ -0,0 +1,37 @@
+package inf.unideb.backend.security;
+
+import inf.unideb.backend.model.User;
+import inf.unideb.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository repo) {
+        this.userRepository = repo;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException(
+                                "User not found: " + username)
+                );
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())  // hashed password
+                .roles("USER")
+                .build();
+    }
+}
