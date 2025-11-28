5dd67f6466e4f267ed53295aa014f3918f068082
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-28 11:24:25 +0100

    fix: checkstyle violation

--- backend/src/main/java/inf/unideb/backend/config/SecurityConfig.java
+++ backend/src/main/java/inf/unideb/backend/config/SecurityConfig.java
@@ -4,20 +4,27 @@ import inf.unideb.backend.security.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security
+        .authentication.AuthenticationManager;
+import org.springframework.security.config
+        .annotation.authentication
+        .configuration.AuthenticationConfiguration;
+import org.springframework.security.config
+        .annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security
+        .config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security
+        .web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
+@Profile("!test")
 public class SecurityConfig {
 
     private final JwtAuthenticationFilter jwtAuthFilter;
@@ -34,18 +41,16 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/api/auth/**",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**"
+                                HttpMethod.OPTIONS, "/**"
                         ).permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(
-                        sess ->
-                        sess
-                                .sessionCreationPolicy(
-                                        SessionCreationPolicy.STATELESS
-                                )
+                        sess -> sess
+                        .sessionCreationPolicy(
+                                SessionCreationPolicy.STATELESS
+                        )
                 )
                 .addFilterBefore(
                         jwtAuthFilter,
@@ -56,7 +61,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
+    public AuthenticationManager authManager(
             AuthenticationConfiguration config
     ) throws Exception {
         return config.getAuthenticationManager();
