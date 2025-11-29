4c5c7e2aae280217eedcf6a41a1b39a4bd79a3b8
Molnar Zsolt Levente <molzsolti03@mailbox.unideb.hu>
2025-11-29 15:21:50 +0100

    fix: board model many to many relation

--- backend/src/main/java/inf/unideb/backend/model/Board.java
+++ backend/src/main/java/inf/unideb/backend/model/Board.java
@@ -1,17 +1,22 @@
 package inf.unideb.backend.model;
 
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.ManyToMany;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.PreRemove;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +37,21 @@ public class Board {
     private String name;
 
     @ManyToOne
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private User user;
 
     @ManyToMany
+    @JoinTable(
+            name = "BOARDS_ITEMS",
+            joinColumns = @JoinColumn(name = "board_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id",
+                    foreignKey = @ForeignKey(name = "fk_item_board"))
+    )
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private List<Item> items = new ArrayList<>();
+
+    @PreRemove
+    private void preRemove() {
+        this.items.clear();
+    }
 }
